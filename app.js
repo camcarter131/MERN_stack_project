3736dc59dc1997628d20a268c95a587c98a76b19
Cameron Carter <cameroncarter@utexas.edu>
2019-05-11 15:58:21 -0700

    Edit out comments from boilerplate app.js

--- app.js
+++ app.js
@@ -2,25 +2,16 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-// import React from 'react';
-// import ReactDOM from 'react-dom';
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Goodbye Cruel World"));
+app.get("/", (req, res) => res.send("Goodbye Beautiful World"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
 
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-
-//     const canvas = document.getElementById("canvas");
-//     ReactDOM.render(<h2>Whoops</h2>, canvas);
-// });
 
