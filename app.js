555569ef51bdbb3b6fdd1adefdf565d9e7158a9b
jinfull <jinfull@gmail.com>
2019-05-11 15:02:40 -0700

    this is my code

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Goodbye Cruel World"));
+app.get("/", (req, res) => res.send("Goodbye Cruel Wrockets suckorld"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
