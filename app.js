00fd3cf8f2ea5ca492f59e5afdc1ce21884cbe54
jinfull <jinfull@gmail.com>
2019-05-11 16:43:26 -0700

    update dependencies in package.json in both

--- app.js
+++ app.js
@@ -11,7 +11,3 @@ mongoose
 app.get("/", (req, res) => res.send("Goodbye Beautiful World"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
-
-
