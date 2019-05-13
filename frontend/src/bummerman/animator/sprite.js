cc30734289f6ee55a44967928888f119333f6e7f
aseemar <ajseemar@gmail.com>
2019-05-12 21:02:23 -0700

    Create resource manager that loads all image assets and allows creation of callbacks that fire in fifo succession after all resources have been loaded into DOM

--- frontend/src/bummerman/animator/sprite.js
+++ frontend/src/bummerman/animator/sprite.js
@@ -1,22 +1,41 @@
+import Input from "../input";
+
 class Sprite {
-    constructor (canvas, ctx, src) {
-        this.img = new Image();
-        this.img.src = src;
+    constructor (canvas, ctx, img) {
         this.canvas = canvas;
         this.ctx = ctx;
+
+        this.img = img;
+
+        this.inputHandler = new Input(this);
 
         this.size = {
             width: 32,
             height: 32
         };
 
-        this.img.onload = this.render.bind(this);
+        this.position = {
+            x: 0,
+            y: 0
+        };
+
+        this.speed = 100;
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+
+    }
+
+    update (dt) {
+        this.position.x += this.velocity.x * dt
+        this.position.y += this.velocity.y * dt
     }
 
     render () {
-        // debugger;
-        this.ctx.drawImage(this.img, 0, 0, this.size.width, this.size.height, 0, 0, this.size.width, this.size.height);
+        this.ctx.drawImage(this.img, 0, 0, this.size.width, this.size.height, this.position.x, this.position.y, this.size.width, this.size.height);
     }
+
 }
 
 export default Sprite;
--- frontend/src/bummerman/game.js
+++ frontend/src/bummerman/game.js
@@ -1,35 +1,47 @@
 import Grid from "./grid";
 import Player from "./player";
 import Sprite from "./animator/sprite";
+import ResourceManager from "./resource_manager/resource_manager";
 
 class Game {
     constructor (canvas, ctx) {
         this.canvas = canvas;
         this.ctx = ctx;
 
+        this.rm = new ResourceManager();
+
         this.grid = new Grid(canvas, ctx);
         this.player = new Player(canvas, ctx);
 
         this.initialTime = Date.now();
 
-        this.start();
+        
+        this.rm.load("assets/images/df_bomber_ss.png");
+        this.rm.onReady(this.init.bind(this));
+        
+        
+        this.init();
+    }
 
-        this.sprite = new Sprite(canvas, ctx, "assets/images/df_bomber_ss.png");
+    init () {
+        this.sprite = new Sprite(this.canvas, this.ctx, this.rm.get("assets/images/df_bomber_ss.png"));
+        this.start();
     }
     
     update (dt) {
         this.player.update(dt);
+        this.sprite.update(dt);
     }
     
     render () {
-        // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         
         this.grid.drawGrid();
         this.grid.renderGame();
         
         this.player.render();
 
-        // this.sprite.render();
+        this.sprite.render();
     }
 
     start () {
