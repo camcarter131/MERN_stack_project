741a262054b1bddd39072bcfd49e06c19c0993d7
Cameron Carter <cameroncarter@utexas.edu>
2019-05-14 19:20:34 -0700

    Clean up comments

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const app = express();
 const serv = require('http').Server(app);
 const db = require('./config/keys').mongoURI;
 
-
 const Game = require('./src/game');
 const Player = require('./src/player');
 const SOCKETS = {};
 const PLAYERS = {};
 const numPlayers = 4;
-
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -33,22 +31,18 @@ const start = () => {
         
 
         let pack = game.update(PLAYERS, dt);
-        // console.log(pack);
+        
         Object.values(SOCKETS).forEach(socket => {
-            // console.log(pack);
             socket.emit('updatePlayer', {pack});
         });
-        // game.render();
     
         game.initialTime = time;
-    }, 1000 / 60) 
-    // requestAnimationFrame(start);
+    }, 1000 / 30) 
 };
 
 io.sockets.on('connection', (socket) => {
     SOCKETS[socket.id] = socket;
     PLAYERS[socket.id] = new Player(game.generateRandomPosition(), game);
-    // console.log(SOCKETS);
     console.log(Object.keys(PLAYERS).length);
 
     if (numPlayers === Object.keys(PLAYERS).length && !gameStarted) {
@@ -58,10 +52,8 @@ io.sockets.on('connection', (socket) => {
     }
 
     socket.on('keysPressed', keys => {
-        // Object.values(PLAYERS).forEach(player => player.handleInput(player.dt, keys));
         const currentPlayer = PLAYERS[socket.id];
         currentPlayer.handleInput(currentPlayer.dt, keys);
-        // console.log(socket.id);
     });
 
     socket.on('disconnect', () => {
@@ -70,69 +62,3 @@ io.sockets.on('connection', (socket) => {
     });
     
 });
-
-
-
-//  ******** Trial 1
-
-
-// function startGame (game) {
-//     let time = Date.now();
-//     let dt = (time - game.initialTime) / 1000.0;
-
-//     game.update(dt);
-//     console.log('loooped')
-//     // this.render();
-
-//     game.initialTime = time;
-//     requestAnimationFrame(startGame);
-// }
-
-
-// ******** Trial 3
-// const game = new Game(2)
-
-// io.sockets.on('connection', (socket) => {
-//     // debugger;
-//     // console.log(socket.id);
-//     SOCKETS[socket.id] = socket;
-//     game.addPlayer(socket.id);
-//     // console.log(document);
-//     // socket.emit('newPlayer', { socket });
-
-//     // socket.on('startGame', data => {
-//     //     startGame(data.game, data.socket)     
-//     // });
-
-//     socket.on('disconnect', () => {
-//         // console.log('socket disconnected');
-//         // socket.emit('playerDisconnect', { socketId: socket.id });
-//         game.deletePlayer(socket.id);
-//     });
-
-//     // socket.emit('startGame', {start: start});
-// });
-
-
-// ***  Trial 2
-// setInterval(() => {
-
-//     Object.keys(game.sockets).forEach(socketId => {
-//         console.log(game.sockets.socketId);
-//         // game.sockets[socketId].player.update(1);
-//         game.sockets.socketId.emit('updatePos', game.sockets[socketId].player.position);
-//     });
-
-
-// }, 1000/30);
-
-// const start = () => {
-//     let time = Date.now();
-//     let dt = (time - game.initialTime) / 1000.0;
-
-//     game.update(dt);
-//     game.render();
-
-//     game.initialTime = time;
-//     requestAnimationFrame(game.start.bind(game));
-// }
